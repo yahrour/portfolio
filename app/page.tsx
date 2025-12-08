62bd627bf51670669aada45a0da827b9105e1b08
0x0bito <iskeyram@gmail.com>
2025-12-08 15:13:15 +0100

    add color variables

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ThemeToggle from "@/components/ThemeToggle";
 
 export default function Home() {
   return (
-    <div className="light:bg-[#666] light:text-black h-screen transition-all duration-300 dark:bg-black dark:text-white">
+    <div className="bg-primary text-foreground h-screen transition-all duration-300">
       <h1 className="text-4xl font-bold">Hello</h1>
       <ThemeToggle />
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,14 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgba(var(--primary))",
+        foreground: "rgba(var(--foreground))",
+        secondary: "rgba(var(--secondary))",
+        bcolor: "rgba(var(--bcolor))",
+      },
+    },
   },
   plugins: [],
 };
