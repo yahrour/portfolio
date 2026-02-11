6d4c12bdab633eb49d5f1492e777ce94603fd5a8
0x0bito <iskeyram@gmail.com>
2026-02-11 23:38:01 +0100

    Change font size for project tags

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -25,7 +25,7 @@ export default function ProjectCard({
     <div className="border-bcolor space-y-4 border-l-2 pl-4">
       <h3 className="text-[0.95rem] font-medium">{title}</h3>
       <p className="text-secondary text-[0.9rem] leading-7">{description}</p>
-      <ul className="flex w-fit flex-wrap items-center gap-2 text-[0.7rem] font-medium">
+      <ul className="flex w-fit flex-wrap items-center gap-2 text-[0.8rem] font-medium">
         {techstack.map((element, idx) => (
           <li key={element} className="flex gap-2">
             <span>{element}</span>
