12058e429f72683c2f0b91c0feaae8380cdf4032
yahrour <139694142+yahrour@users.noreply.github.com>
2026-08-14 00:07:46 +0100

    feat: add home page content

---
+++ src/constants/socials.ts
@@ -0,0 +1,5 @@
+export const socials = {
+  email: "youssefahrour.contact@gmail.com",
+  github: "https://github.com/yahrour",
+  linkedin: "https://linkedin.com/in/youssef-ahrour-2177a6383",
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,49 @@
+import { Link } from "react-router";
+import { socials } from "../constants";
+import { CopyToClipboard } from "../components/CopyToClipboard";
+
 export default function Home() {
   return (
     <div className="w-full space-y-10">
-      <h1>Home</h1>
+      <div className="w-full space-y-10">
+        <h2 className="text-xl font-medium">Get to Know Me</h2>
+        <div className="space-y-4">
+          <h3 className="text-lg font-medium">
+            I&apos;m Youssef Ahrour, Backend developer.
+          </h3>
+          <p className="text-secondary text-[0.9rem] leading-7">
+            Currently studying at 1337 (a peer-to-peer, project-based
+            engineering school part of the 42 Network), where I focus on systems
+            programming and backend development. I&apos;m actively looking for
+            internship or junior developer opportunities — comfortable working
+            remotely and ready to contribute to a team building meaningful
+            solutions.
+          </p>
+          <p className="text-secondary text-[0.9rem] leading-7">
+            Currently working on a project that requires me to dive into{" "}
+            <span className="text-foreground font-medium">NestJS</span>,
+            strengthening my backend development skills and understanding of its
+            ecosystem.
+          </p>
+        </div>
+      </div>
+      <div className="space-x-8 text-sm">
+        <Link to={socials.linkedin} className="underline" target="_blank">
+          Linkedin
+        </Link>
+        <div className="inline-block space-x-2">
+          <Link
+            to={`mailto:${socials.email}`}
+            className="inline-block underline"
+          >
+            Email
+          </Link>
+          <CopyToClipboard content={socials.email} />
+        </div>
+        <Link to={socials.github} className="underline" target="_blank">
+          Github
+        </Link>
+      </div>
     </div>
   );
 }
