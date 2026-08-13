3ebb8064084c7371372542ef76c43956ceb129c8
yahrour <139694142+yahrour@users.noreply.github.com>
2026-08-14 00:37:30 +0100

    feat: add contact page content

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -1,5 +1,17 @@
-export const socials = {
-  email: "youssefahrour.contact@gmail.com",
-  github: "https://github.com/yahrour",
-  linkedin: "https://linkedin.com/in/youssef-ahrour-2177a6383",
-};
+export const socials = [
+  {
+    name: "Email",
+    href: "youssefahrour.contact@gmail.com",
+    label: "youssefahrour.contact@gmail.com",
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/yahrour",
+    label: "github.com/yahrour",
+  },
+  {
+    name: "LinkedIn",
+    href: "https://linkedin.com/in/youssef-ahrour-2177a6383",
+    label: "linkedin.com/in/youssef-ahrour",
+  },
+];
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,46 @@
+import { Link } from "react-router";
+import { CopyToClipboard } from "../components/CopyToClipboard";
+import { socials } from "../constants";
+
 export default function Contact() {
   return (
     <div className="w-full space-y-10">
-      <h1>Contact</h1>
+      <h2 className="text-xl font-medium">Let’s Connect</h2>
+      <div className="space-y-5">
+        <p className="text-secondary text-[0.9rem] leading-7">
+          I’m open to full-time roles, internships, and freelance work. For
+          opportunities or direct contact, reach out.
+        </p>
+      </div>
+      <div className="text-sm">
+        <ul className="space-y-4">
+          {socials.map((social) => {
+            return (
+              <li className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-1">
+                <span className="font-medium text-secondary">
+                  {social.name}
+                </span>
+                <div className="flex items-center gap-2 mt-1 sm:mt-0">
+                  <Link
+                    to={social.href}
+                    className="underline wrap-break-word mt-1 sm:mt-0"
+                    target="_blank"
+                  >
+                    {social.label}
+                  </Link>
+                  {social.name === "Email" ? (
+                    <span className="inline-block">
+                      <CopyToClipboard content={social.href} />
+                    </span>
+                  ) : (
+                    <span className="inline-block w-3.5"></span>
+                  )}
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
