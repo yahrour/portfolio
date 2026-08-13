0e9f67ef53c6df9b5afb10a53f36b003ef966781
yahrour <139694142+yahrour@users.noreply.github.com>
2026-08-14 00:10:38 +0100

    feat: add projects page content

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,55 @@
+import { ArrowRight } from "lucide-react";
+import { Link } from "react-router";
+
+type linkObject =
+  | {
+      title: string;
+      href: string;
+    }
+  | undefined;
+
+type propsType = {
+  title: string;
+  description: string;
+  techStack: string[];
+  links: linkObject[];
+};
+
+export default function ProjectCard({
+  title,
+  description,
+  techStack,
+  links,
+}: propsType) {
+  return (
+    <div className="border-bcolor space-y-4 border-l-2 pl-4">
+      <h3 className="text-[0.95rem] font-medium">{title}</h3>
+      <p className="text-secondary text-[0.9rem] leading-7">{description}</p>
+      <ul className="flex w-fit flex-wrap items-center gap-2 text-[0.8rem] font-medium">
+        {techStack.map((element, idx) => (
+          <li key={element} className="flex gap-2">
+            <span>{element}</span>
+            {techStack.length - 1 != idx && "·"}
+          </li>
+        ))}
+      </ul>
+      <ul className="flex w-fit items-center gap-4 text-sm">
+        {links?.map((element) => (
+          <li key={element?.href}>
+            <Link
+              to={element?.href || ""}
+              className="group flex items-center gap-1 underline"
+              target="_blank"
+            >
+              {element?.title}
+              <ArrowRight
+                size={12}
+                className="transition-all duration-300 group-hover:translate-x-1"
+              />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/constants/projects.ts
@@ -0,0 +1,65 @@
+export const projects = [
+  {
+    title: "Receipt-Flow: AI-Powered Expense Management System",
+    description:
+      "An AI-native engine that automates the transition from paper to data. I built a seamless pipeline that converts raw images into structured PostgreSQL records, eliminating manual entry. Powered by Google Gemini 2.5 Flash, the system instantly extracts merchant details, totals, and dates, enabling users to manage expenses with high-speed precision.",
+    techStack: [
+      "React",
+      "Express.js",
+      "Gemini 2.5 Flash",
+      "TanStack Query",
+      "TypeScript",
+      "PostgreSQL",
+      "Better-Auth",
+      "Docker",
+    ],
+    links: [
+      {
+        title: "view source code",
+        href: "https://github.com/yahrour/receipt-flow",
+      },
+    ],
+  },
+  {
+    title: "Digital Closet",
+    description:
+      "A wardrobe organizer where users can save items with images, categories, colors, and tags, then combine them into outfits. Built with Next.js, PostgreSQL, and AWS S3, with secure authentication and fully user-scoped data.",
+    techStack: ["Next.js", "AWS S3", "PostgreSQL", "Better-Auth", "Docker"],
+    links: [
+      {
+        title: "view source code",
+        href: "https://github.com/yahrour/digital-closet",
+      },
+    ],
+  },
+  {
+    title: "Product Nutrition Scanner",
+    description:
+      "A web app that scans product barcode and displays nutrition information with color-coded health indicators.",
+    techStack: [
+      "Next.js",
+      "TypeScript",
+      "Quagga2",
+      "Open Food Facts API",
+      "Docker",
+    ],
+    links: [
+      {
+        title: "view source code",
+        href: "https://github.com/yahrour/product-nutrition-scanner",
+      },
+    ],
+  },
+  {
+    title: "Tag-Based Email Manager",
+    description:
+      "A full-stack email management application with tag-based organization and search functionality.",
+    techStack: ["React", "TypeScript", "Express.js", "PostgreSQL", "Docker"],
+    links: [
+      {
+        title: "view source code",
+        href: "https://github.com/yahrour/tag-based-email-manager",
+      },
+    ],
+  },
+];
